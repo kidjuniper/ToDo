963b4fc5c4bdfba2ac0117cbf3d2433a4599253a
kidjuniper <kidjuniper@yandex.ru>
2024-09-20 01:57:26 +0300

    updated animation time

--- ToDo/ToDoList/Modules/Loading/LoadingViewController.swift
+++ ToDo/ToDoList/Modules/Loading/LoadingViewController.swift
@@ -64,8 +64,8 @@ extension LoadingViewController {
 // MARK: - LoadingViewProtocol extension
 extension LoadingViewController: LoadingViewProtocol {
     func stopAnimation(completion: @escaping () -> Void) {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
-            UIView.animate(withDuration: 5,
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            UIView.animate(withDuration: 0.75,
                            animations: {
                 self.animationView.layer.opacity = 0
             }) { _ in
