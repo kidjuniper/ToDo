31463afdb12a06b430c582d87c9536aff21896cf
kidjuniper <kidjuniper@yandex.ru>
2024-09-24 20:27:03 +0300

    bag fixed

--- ToDo/ToDoList/Core/Network/Models/Tasks.swift
+++ ToDo/ToDoList/Core/Network/Models/Tasks.swift
@@ -21,7 +21,7 @@ struct TaskModel: Codable,
     var todo: String
     @DefaultFalse
     var completed: Bool
-    @DefaultCommentString
+    @DefaultEmptyString
     var comment: String
     @DefaultStartDate
     var startDate: Date
